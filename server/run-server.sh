9ad72467288079a6f8574ac5ecd1cfde96ac880c
Gregory Huczynski <gh_online@me.com>
2015-08-13 11:34:39 +0100

    Ensure run-server.sh runs in server dir.

--- server/run-server.sh
+++ server/run-server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-# Start minecraft server
+SERVER_DIR=`readlink -f "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"`
+
+cd ${SERVER_DIR}
 java -Xmx1024M -jar ../lib/CanaryMod-1.8.0-1.2.1-SNAPSHOT-shaded.jar -o true
